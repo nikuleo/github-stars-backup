6e884395a61a2f2985fc2a4a612562037748e450
niku <nikuliu@foxmail.com>
2023-03-02 20:13:03 +0800

    fix bug: add description field

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type RepoOwner struct {
 	Name string `json:"login"`
 	URL  string `json:"html_url"`
 }
-type repoData struct {
+type RepoData struct {
 	Name        string    `json:"name"`
 	URL         string    `json:"html_url"`
 	Description string    `json:"description"`
@@ -29,10 +29,10 @@ type repoData struct {
 
 type RepoYAML struct {
 	Name        string `yaml:"repo name"`
-	Owner       string `yaml:"author"`
-	OwnerURL    string `yaml:"author's github"`
 	URL         string `yaml:"url"`
 	Description string `yaml:"description"`
+	Owner       string `yaml:"author"`
+	OwnerURL    string `yaml:"author's github"`
 	Language    string `yaml:"language"`
 	Topics      string `yaml:"tags"`
 }
@@ -49,28 +49,29 @@ func main() {
 	flag.Parse()
 
 	if username == "" {
-		fmt.Println("please input your github username.")
+		log.Println("please input your github username.")
 		return
 	}
 	max, err := strconv.Atoi(maxRepo)
 	if err != nil {
-		log.Printf("change \"maxRepo\" type error: %v", err)
+		log.Fatalf("change \"maxRepo\" type error: %v", err)
 		return
 	}
 	allRepos, err := getStarredRepo(username, max)
 	if err != nil {
-		log.Printf("get repos error: %v", err)
+		log.Fatalf("get repos error: %v", err)
 		return
 	}
 
 	var repos []RepoYAML
 	for _, v := range allRepos {
 		repo := RepoYAML{
-			Name:     v.Name,
-			Owner:    v.Owner.Name,
-			OwnerURL: v.Owner.URL,
-			URL:      v.URL,
-			Language: v.Language,
+			Name:        v.Name,
+			URL:         v.URL,
+			Description: v.Description,
+			Owner:       v.Owner.Name,
+			OwnerURL:    v.Owner.URL,
+			Language:    v.Language,
 		}
 		for k, t := range v.Topics {
 			repo.Topics += t
@@ -87,13 +88,13 @@ func main() {
 
 	if savePath != "" {
 		if _, err := os.Stat(savePath); errors.Is(err, os.ErrNotExist) {
-			err := os.Mkdir(savePath, os.ModePerm)
+			err := os.Mkdir(savePath, 0700)
 			if err != nil {
-				log.Println(err)
+				log.Fatalf("create dir error: %v", err)
 			}
 		}
 	}
-	path := savePath + "stars.yaml"
+	path := savePath + "/stars.yaml"
 	err = ioutil.WriteFile(path, yamlData, 0644)
 	if err != nil {
 		log.Fatalf("write file error: %v", err)
@@ -103,29 +104,26 @@ func main() {
 	fmt.Printf("You successfully backed up %d starred repos\n", len(allRepos))
 }
 
-func getStarredRepo(username string, maxRepo int) ([]repoData, error) {
+func getStarredRepo(username string, maxRepo int) ([]RepoData, error) {
 	var (
 		repoCnt  int
 		pageIdx  = 1
-		allRepos []repoData
+		allRepos []RepoData
 	)
 
 	for {
 		resp, err := http.Get(fmt.Sprintf("https://api.github.com/users/%s/starred?page=%d&per_page=%d", username, pageIdx, 30))
 		if err != nil {
-			log.Printf("get stars error: %v", err)
-			return nil, err
+			return nil, fmt.Errorf("get stars error: %w", err)
 		}
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
-			log.Printf("read body error: %v", err)
-			return nil, err
+			return nil, fmt.Errorf("read body erorr: %w", err)
 		}
 
-		var jsonData []repoData
+		var jsonData []RepoData
 		if err := json.Unmarshal(body, &jsonData); err != nil {
-			log.Printf("json unmarshal error: %v", err)
-			return nil, err
+			return nil, fmt.Errorf("json unmarshal error: %w", err)
 		}
 		repoCnt += len(jsonData)
 		allRepos = append(allRepos, jsonData...)
